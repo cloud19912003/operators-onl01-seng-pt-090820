d5e8ddb5bbc9759844831199fe0cf218f889ec4f
Michael Murray <cloud19912003@live.com>
2020-09-13 20:19:16 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,7 +12,7 @@ end
 
 def not_safe?(speed)
 	return true unless speed < 40 ? false : true
-	return false unless speed > 60 ? false  : true
+	return true unless speed > 60 ? false  : true
 end
 	
 
